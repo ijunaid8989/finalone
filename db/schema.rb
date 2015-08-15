1f5122bd8b4494efd30412721ecb5159d1beceab
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-08-15 15:35:01 -0700

    Added two models one for level and other is for subs and migrated as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814233444) do
+ActiveRecord::Schema.define(version: 20150815223222) do
+
+  create_table "levels", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "level_id"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   limit: 255
